27c56fd0d9d3ebdbf29bc71ad86d355b18bd3aab
mmelly24 <mmelly24@users.noreply.github.com>
2021-05-31 17:00:24 +0200

    Bouton 'back to menu' dans SceneInstructions

--- scenes/SceneInstructions.js
+++ scenes/SceneInstructions.js
@@ -7,7 +7,12 @@ class SceneInstructions extends Phaser.Scene{
     create() {
         let boutonNext = this.add.text(700, 500, 'NEXT');
         boutonNext.setInteractive();
-        boutonNext.on('pointerdown', () => this.scene.start('mariage'))
+        boutonNext.on('pointerdown', () => this.scene.start('mariage'));
+
+        let boutonMenu = this.add.text(200, 100, 'BACK TO MENU');
+        boutonMenu.setInteractive();
+        boutonMenu.on('pointerdown', () => this.scene.start('accueil'));
+        
     }
 }
 
