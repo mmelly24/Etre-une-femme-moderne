79606a8e7d3d155af05a72d271ffeefc7b9f7df1
mmelly24 <mmelly24@users.noreply.github.com>
2021-06-02 14:40:29 +0200

    insertion sprite menu

--- scenes/SceneMenu.js
+++ scenes/SceneMenu.js
@@ -4,12 +4,12 @@ class SceneMenu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('femme', 'assets/Femme_accueil.jpg');
+    this.load.image('femme', 'assets/sprite_working_woman1.png');
   }
 
   create() {
     this.add.text(400, 100, 'ETRE UNE FEMME MODERNE');
-    this.add.image(500, 300, 'femme');
+    this.add.image(500,300, 'femme').setScale(0.7)
     let boutonPlay = this.add.text(480, 500, 'PLAY');
     boutonPlay.setInteractive();
     boutonPlay.on('pointerdown', () => this.scene.start('instructions'));
