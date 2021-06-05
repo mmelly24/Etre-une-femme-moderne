d14efec6784e91f0f7ffae46ad238c2637b077ef
mmelly24 <mmelly24@users.noreply.github.com>
2021-06-05 14:03:58 +0200

    série de NEXT dans SceneGrossesse

--- scenes/SceneGrossesse.js
+++ scenes/SceneGrossesse.js
@@ -1,19 +1,25 @@
 class SceneGrossesse extends Phaser.Scene {
-    constructor() {
-      super('grossesse');
-    }
+  constructor() {
+    super('grossesse');
+  }
 
-    create(){
-      //texte à implémenter
-      this.add.text(380, 300, 'annonce:blablabl')
+  create() {
+    //test
+    let text;
+    let count = 0;
 
-        let boutonAnnonce = this.add.text(380, 500, 'NEXT');
-        boutonAnnonce.setInteractive();
-        boutonAnnonce.on('pointerdown', function(){
-          console.log('bonjour');
-        });
+    text = this.add.text(380, 300, 'annonce: blablabla');
 
-
-
-    }
+    let boutonAnnonce = this.add.text(380, 500, 'NEXT');
+    boutonAnnonce.setInteractive();
+    //exemple d'une fonctionnalité NEXT
+    boutonAnnonce.on('pointerdown', () => {
+      count++;
+      if (count == 1) {
+        text.setText('Tout le monde est heureux pour vous');
+      } else if (count == 2) {
+        text.setText('Vous commencez à vous sentir très fatiguée');
+      }
+    });
+  }
 }
