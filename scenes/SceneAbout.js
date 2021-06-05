bef8f68f09d6e566be54ca7a38d70efeeb97d080
mmelly24 <mmelly24@users.noreply.github.com>
2021-06-05 14:46:38 +0200

    tentative centrage text

--- scenes/SceneAbout.js
+++ scenes/SceneAbout.js
@@ -1,11 +1,18 @@
 class SceneAbout extends Phaser.Scene {
-    constructor() {
-      super('about');
-    }
+  constructor() {
+    super('about');
+  }
 
-    create(){
-              let boutonMenu = this.add.text(200, 100, 'BACK TO MENU');
-        boutonMenu.setInteractive();
-        boutonMenu.on('pointerdown', () => this.scene.start('accueil'));
-    }
+  create() {
+
+    const text = "Projet créé dans le cadre d'un cours donné à l'Université de Lausanne.";
+
+    const contenu = this.add.text(0, 0, text)
+
+    Phaser.Display.Align.In.Center(contenu, this.add.zone(600, 300, 800, 600));
+
+    let boutonMenu = this.add.text(200, 100, 'BACK TO MENU');
+    boutonMenu.setInteractive();
+    boutonMenu.on('pointerdown', () => this.scene.start('accueil'));
+  }
 }
