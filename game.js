814b6ee08ca7174da5f62a5de4bac86ca08f0b95
npio <73123157+npio-unil@users.noreply.github.com>
2021-06-07 19:38:13 +0200

    classe TextBox
    ajout de la classe TextBox pour optimiser l'affichage du texte

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
-
 const config = {
   width: 1000,
   height: 600,
   type: Phaser.WEBGL,
-  backgroundColor : '#653592', //couleur test
+  backgroundColor: '#653592', //couleur test
   physics: {
     default: 'arcade',
     arcade: {
-      debug: false
-    }
+      debug: false,
+    },
   },
 
   //ajouter les autres scènes au fur et à mesure
@@ -30,7 +29,111 @@ const config = {
   ],
 };
 
+class TextBox extends Phaser.GameObjects.Container {
+  constructor(scene, content) {
+    super(scene);
+
+    createTextBox(scene, 200, 200, {
+      wrapWidth: 400,
+    }).start(content, 50);
+  }
+}
+
+const GetValue = Phaser.Utils.Objects.GetValue;
+const COLOR_PRIMARY = 000000;
+const COLOR_LIGHT = 000000;
+const COLOR_DARK = 000000;
+
+function createTextBox(scene, x, y, config) {
+  let wrapWidth = GetValue(config, 'wrapWidth', 0);
+  let fixedWidth = GetValue(config, 'fixedWidth', 0);
+  let fixedHeight = GetValue(config, 'fixedHeight', 0);
+  console.log(scene.rexUI);
+  let textBox = scene.rexUI.add
+    .textBox({
+      x: x,
+      y: y,
+
+      background: scene.rexUI.add
+        .roundRectangle(0, 0, 2, 2, 20, COLOR_PRIMARY)
+        .setStrokeStyle(2, COLOR_LIGHT),
+
+      icon: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_DARK),
+
+      // text: getBuiltInText(scene, wrapWidth, fixedWidth, fixedHeight),
+      text: getBBcodeText(scene, wrapWidth, fixedWidth, fixedHeight),
+
+      action: scene.add
+        .image(0, 0, 'nextPage')
+        .setTint(COLOR_LIGHT)
+        .setVisible(false),
+
+      space: {
+        left: 20,
+        right: 20,
+        top: 20,
+        bottom: 20,
+        icon: 10,
+        text: 10,
+      },
+    })
+    .setOrigin(0)
+    .layout();
+
+  textBox
+    .setInteractive()
+    .on(
+      'pointerdown',
+      function () {
+        let icon = this.getElement('action').setVisible(false);
+        this.resetChildVisibleState(icon);
+        if (this.isTyping) {
+          this.stop(true);
+        } else {
+          this.typeNextPage();
+        }
+      },
+      textBox
+    )
+    .on(
+      'pageend',
+      function () {
+        if (this.isLastPage) {
+          return;
+        }
+
+        let icon = this.getElement('action').setVisible(true);
+        this.resetChildVisibleState(icon);
+        icon.y -= 30;
+        let tween = scene.tweens.add({
+          targets: icon,
+          y: '+=30', // '+=100'
+          ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+          duration: 500,
+          repeat: 0, // -1: infinity
+          yoyo: false,
+        });
+      },
+      textBox
+    );
+  //.on('type', function () {
+  //})
+
+  return textBox;
+}
+
+function getBBcodeText(scene, wrapWidth, fixedWidth, fixedHeight) {
+  return scene.rexUI.add.BBCodeText(0, 0, '', {
+    fixedWidth: fixedWidth,
+    fixedHeight: fixedHeight,
+
+    fontSize: '20px',
+    wrap: {
+      mode: 'word',
+      width: wrapWidth,
+    },
+    maxLines: 6,
+  });
+}
+
 let jeu = new Phaser.Game(config);
-
-
- 
--- scenes/SceneInstructions.js
+++ scenes/SceneInstructions.js
@@ -1,28 +1,39 @@
-const COLOR_PRIMARY = 000000;
+/*const COLOR_PRIMARY = 000000;
 const COLOR_LIGHT = 000000;
-const COLOR_DARK = 000000;
+const COLOR_DARK = 000000;*/
 
 let content = `Vous incarnez une jeune femme de 25 ans, nommée Julie. Après un parcours universitaire exemplaire, vous entrez dans le monde du travail, plus motivée que jamais. 
 Quelques mois de recherche ont suffi à ce que vous trouviez un job, qui répond à toutes vos attentes. Vous avez hâte de commencer, et de rencontrer tous vos nouveaux collègues.`;
 
-class SceneInstructions extends Phaser.Scene{
-    constructor(){
-        super('instructions')
-    }
+class SceneInstructions extends Phaser.Scene {
+  constructor() {
+    super('instructions');
+  }
 
-    preload() { 
-        this.load.image('happyFace', 'assets/sprite_happy_face.png')
-        this.load.scenePlugin({
-            key: 'rexuiplugin',
-            url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
-            sceneKey: 'rexUI'
-        });
-        
-        this.load.image('nextPage', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/assets/images/arrow-down-left.png');
-    }
+  preload() {
+    this.load.image('happyFace', 'assets/sprite_happy_face.png');
+    this.load.image('happyFace', 'assets/sprite_happy_face.png');
+    this.load.scenePlugin({
+      key: 'rexuiplugin',
+      url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
+      sceneKey: 'rexUI',
+    });
+    this.load.image(
+      'nextPage',
+      'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/assets/images/arrow-down-left.png'
+    );
+  }
 
-    create() {
+  create() {
+    this.add.image(700, 150, 'happyFace').setScale(0.5);
 
+    let boutonMenu = this.add.text(200, 100, 'BACK TO MENU');
+    boutonMenu.setInteractive();
+    boutonMenu.on('pointerdown', () => this.scene.start('accueil'));
+    changerPage(this, 700, 500, 'NEXT');
+    new TextBox(this, content);
+
+    /*
         this.add.image(700, 150, 'happyFace').setScale(0.5)
 
         let boutonMenu = this.add.text(200, 100, 'BACK TO MENU');
@@ -33,23 +44,20 @@ class SceneInstructions extends Phaser.Scene{
                 wrapWidth: 400,
             })
             .start(content, 50);
+      
+       */
+  }
 
-        
-       changerPage(this, 700, 500, 'NEXT');
-    }
-
-    update() {
-    }
+  update() {}
 }
 
 function changerPage(scene, x, y, contenu) {
-    let boutonNext = scene.add.text(x, y, contenu);
-    boutonNext.setInteractive();
-    boutonNext.on('pointerdown', () => scene.scene.start('mariage'));
-    return boutonNext;
-
+  let boutonNext = scene.add.text(x, y, contenu);
+  boutonNext.setInteractive();
+  boutonNext.on('pointerdown', () => scene.scene.start('mariage'));
+  return boutonNext;
 }
-
+/*
 const GetValue = Phaser.Utils.Objects.GetValue;
 
 function createTextBox(scene, x, y, config) {
@@ -131,8 +139,7 @@ function getBBcodeText (scene, wrapWidth, fixedWidth, fixedHeight) {
         },
         maxLines: 6
     })
-
-
+*/
 
 /*  create() {
 
@@ -147,5 +154,3 @@ function getBBcodeText (scene, wrapWidth, fixedWidth, fixedHeight) {
         boutonMenu.on('pointerdown', () => this.scene.start('accueil'));
         
     }*/
-}
-
