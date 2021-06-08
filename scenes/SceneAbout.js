5f072222bd69828b73c96e26566bedc3155032dc
mmelly24 <mmelly24@users.noreply.github.com>
2021-06-08 13:56:19 +0200

    mise à la ligne texte about

--- scenes/SceneAbout.js
+++ scenes/SceneAbout.js
@@ -7,7 +7,7 @@ class SceneAbout extends Phaser.Scene {
   create() {
     const style = { fontSize: '18px', wrapWidth: 300, fixedWidth: 300, fixedHeight: 150 }
     let text =
-      "Projet créé dans le cadre du cours intitulé 'Jeu vidéo 2d' et dispensé à l'Université de Lausanne par le professeur Isaac Pante.";
+      "Projet créé dans le cadre du cours intitulé\n 'Jeu vidéo 2d' et dispensé à l'Université de Lausanne par le professeur Isaac Pante.";
     this.add.text(200, 300, text, style);
 
 
