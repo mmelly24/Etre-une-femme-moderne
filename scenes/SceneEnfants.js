8aee8b24a7a939434170d244fd21b6189a2f5b28
mmelly24 <mmelly24@users.noreply.github.com>
2021-06-10 14:16:14 +0200

    mise au propre

--- scenes/SceneEnfants.js
+++ scenes/SceneEnfants.js
@@ -18,8 +18,9 @@ class SceneEnfants extends Phaser.Scene {
       "La décision est claire, tant pour vous que pour votre conjoint: c'est oui! BLA BLA BLA";
     let reponseNon = 'Après mûre réflexion, non. BLA BLA BLA';
     let popupTitre = 'PARENTALITÉ';
-    let popupTexte = "Seules x% des femmes en Suisse n'ont pas d'enfants au cours de leur vie";
-
+    let popupTexte =
+      "Seules x% des femmes en Suisse n'ont pas d'enfants au cours de leur vie";
+    let popupTexte2 = 'test popup 2';
     let textBoxQuestion = creerTextBox(this, content, 3);
 
     let boutonOui = creerBouton(this, false, 380, 500, 'OUI');
@@ -32,77 +33,27 @@ class SceneEnfants extends Phaser.Scene {
       }
     });
 
-    choixJoueur(this, boutonOui, true, textBoxQuestion, reponseOui, 7, boutonNon, 'grossesse', popupTexte)
-   /* boutonOui.on('pointerdown', () => {
-      textBoxQuestion.destroy();
-      reponseTextBox = creerTextBox(this, reponseOui, 7);
-      reponseTextBox.on('pageend', () => {
-        if (reponseTextBox.isLastPage) {
-          if (popupTexte != null) {
-            this.time.addEvent({
-              delay: 5000,
-              callback: () => {
-                reponseTextBox.destroy();
-                var toast = this.rexUI.add.toast({
-                  x: 400,
-                  y: 300,
-                
-                  background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_PRIMARY),
-                  text: this.add.text(0, 0, '', {
-                      fontSize: '24px'
-                  }),
-                  space: {
-                      left: 20,
-                      right: 20,
-                      top: 20,
-                      bottom: 20,
-                  },
-                })
-                  .show(popupTexte)
-                  .show('Phaser 3 is good')
-                  .show('See you next time')
-                
-                changerPage(this, 700, 500, 'NEXT', 'grossesse');
-              },
-              loop: false,
-            });
-          } else changerPage(this, 700, 500, 'NEXT', 'grossesse');
-        }
-      });
-      boutonOui.setVisible(false);
-      boutonNon.setVisible(false);
-      boutonOui.disableInteractive();
-      boutonNon.disableInteractive();
-    });*/
-
-
-
-    //boutonNon.on('pointerdown', () => {});
-
-    /*boutonOui.on('pointerdown', () => {
-      choixJoueur(this, boutonOui, true, textBoxQuestion, reponseOui, 3, boutonNon, 'grossesse', popupTexte, popupTitre);
-    });
-
-    boutonNon.on('pointerdown', () => {
-      choixJoueur(this, boutonNon, false, textBoxQuestion, reponseNon, 3, boutonOui, 'promotion', popupTexte, popupTitre);
-    });*/
-
-    /*
-    this.add.text(380, 300, 'enfants: blablabla');
-
-    let oui = this.add.text(380, 500, 'OUI');
-    oui.setInteractive();
-    //aller vers une scène qui contient de la théorie sur la grossesse
-    oui.on('pointerdown', () => this.scene.start('grossesse'));
-
-    let boutonNon = this.add.text(580, 500, 'NON');
-    boutonNon.setInteractive();
-    //aller vers une scène qui illustre la pression sociale
-    boutonNon.on('pointerdown', () => this.scene.start('pression'));
-    */
+    choixJoueur(
+      this,
+      boutonOui,
+      true,
+      textBoxQuestion,
+      reponseOui,
+      7,
+      boutonNon,
+      'grossesse',
+      popupTexte
+    );
+    choixJoueur(
+      this,
+      boutonNon,
+      true,
+      textBoxQuestion,
+      reponseNon,
+      7,
+      boutonOui,
+      'grossesse',
+      popupTexte
+    );
   }
-
-  
 }
-
-
