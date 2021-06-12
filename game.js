3ecd1d847a383ac69bc6bff851e83013d2a1c4f7
npio <73123157+npio-unil@users.noreply.github.com>
2021-06-12 11:55:59 +0200

    suite jauge désillusion

--- game.js
+++ game.js
@@ -197,36 +197,41 @@ function timeBar(scene, x, y) {
   bar.mask = new Phaser.Display.Masks.BitmapMask(scene, scene.barMask);
 
   let stepWidth = scene.barMask.displayWidth / countBar;
+  //console.log(scene.barMask.displayWidth);
+  //console.log(stepWidth);
 
   countBar *= 1.2; //A DETERMINER EN FONCTION DU NOMBRE DE FOIS QUE L'ON VA APPELER CETTE FONCTION
 
   scene.barMask.x -= stepWidth;
   //bar.x = stepWidth
+  //console.log(scene.barMask.x);
 
   return bar + barContainer;
 }
 
 //------- JAUGE DE DÉSILLLUSION -------
+let countBarD = 1;
 function jaugeDesillusion(scene, x, y, imageSprite, imageBar) {
   scene.add.image(913, 100, imageSprite).setScale(0.4);
-  scene.zone = scene.add.zone((x = 913), (y = config.height / 2.05));
+  //scene.zone = scene.add.zone((x = 900), (y = config.height / 2.05)).setScale(0.7);
   let barContainer = scene.add.image(x, y, 'containerBar').setScale(0.7);
   barContainer.angle = 90;
   let bar = scene.add.image(x+3, y+5, imageBar).setScale(0.7);
+  //bar.setVisible(false);
   bar.angle = 90;
 
-  scene.barMask = scene.add.sprite(bar.x, bar.y, imageBar).setScale(0.7);
+  scene.barMask = scene.add.sprite(200, 300, imageBar).setScale(0.7);
   scene.barMask.angle = 90;
-  scene.barMask.visible = false;
+  scene.barMask.visible = true;
   bar.mask = new Phaser.Display.Masks.BitmapMask(scene, scene.barMask);
 
-  let stepHeight = scene.barMask.displayHeight / countBar;
-  console.log(stepHeight);
+  let stepHeight = scene.barMask.displayHeight / countBarD;
+  //console.log(stepHeight);
 
-  countBar *= 2; //A DETERMINER EN FONCTION DU NOMBRE DE FOIS QUE L'ON VA APPELER CETTE FONCTION
+  countBarD *= 2; //A DETERMINER EN FONCTION DU NOMBRE DE FOIS QUE L'ON VA APPELER CETTE FONCTION
 
   scene.barMask.y -= stepHeight;
-  console.log(scene.barMask.y)
+  //console.log(scene.barMask.y)
   //bar.x = stepWidth
 
   return bar + barContainer;
