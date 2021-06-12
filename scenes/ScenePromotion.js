c55d139f8c23f69aedb987c363074aae8c67d5de
npio <73123157+npio-unil@users.noreply.github.com>
2021-06-12 14:53:55 +0200

    optimisation bouton SUIVANT + ajout des textes

--- scenes/ScenePromotion.js
+++ scenes/ScenePromotion.js
@@ -13,33 +13,29 @@ class ScenePromotion extends Phaser.Scene {
     //this.add.image(700, 150, 'happyFace').setScale(0.5);
     timeBar(this, 500, 400);
 
-    let content =`Votre travail est très prenant, mais il vous enrichit énormément. Il a beaucoup de sens pour vous, vous avez le sentiment de faire avancer votre domaine. Vos chefs sont très contents de votre travail. C’est gratifiant!
-    Les années passent et votre importance grandit au sein de la boîte. Vous avez gagné en responsabilités mais votre salaire n’a pas évolué en conséquence. Les bruits de couloir laissent aussi entendre qu’on vous paie moins que vos homologues masculins. “C’est la relève, il faut qu’on puisse les garder dans l’entreprise.” C’est ce que vous explique votre chef quand vous abordez timidement le sujet. Vous réalisez alors qu’on n’envisage même pas de vous promouvoir, vous! Mais vous allez leur montrer votre valeur! Presque sur un coup de tête, vous demandez à être promue. “Ah mais tu ne veux pas d’enfants? Ce sera vraiment dur de t’en occuper si tu deviens manager, c’est pour ça que je ne te l’ai jamais proposé. Je ne voulais pas te placer face à ce choix difficile!”
-    Vous clarifiez le malentendu et votre chef finit par vous octroyer le poste! Il a toujours cru en vos capacités. Les négociations salariales vous déçoivent un peu, vous soupçonnez que votre salaire reste en-dessous de la norme, mais vous vous estimez déjà chanceuse d’avoir obtenu le poste. ça prouvera qu’une femme peut faire le travail tout aussi bien qu’un homme! La reconnaissance salariale suivra.
-    Vous devenez donc cheffe de projet et prenez en charge la direction d’une petite équipe que vous connaissez bien, puisque ce sont vos anciens collègues. Vous êtes aux anges, votre carrière décolle enfin, après presque 10 ans de dur travail!
-    Mais vous déchantez vite. Ceux qui sont désormais vos collaborateurs changent de comportement. ça commence par des réactions surprises et vexées à votre promotion. Ce sont tous des hommes, et ils vous font rapidement comprendre que la seule explication possible est d’être “passée sous la table” du chef. L’ambiance se détériore rapidement: ils ne vous écoutent pas, ils ne respectent pas vos décisions, ils vous excluent des réunions.
-    “Il faut savoir s’imposer”, vous dit votre supérieur au moment où vous lui confiez le problème. “Les gars, faut leur montrer qui est l’alpha si on veut se faire respecter. Fait preuve d’autorité et de fermeté, ça passera.” Alors c’est ce que vous faites: vous vous grandissez, vous les réprimandez, vous parlez plus fort qu’eux. Mais c’est l’escalade: vous avez “un balais dans le cul”, vous êtes “chiante”, une “connasse”, une “mal baisée”.
-    La situation devient insupportable. Vous avez la boule au ventre en partant travailler, vous subissez stoïquement les insultes et les menaces pendant la journée, et vous vous effondrez le soir. Mathieu est désemparé. “Il faut les dénoncer! Tu peux pas te laisser traiter comme ça, c’est pas normal à notre époque!” Vous y réfléchissez sérieusement. Il faut que quelque chose change, c’est sûr, mais vous avez peur pour votre réputation et votre carrière...`;
+    let content =`dénonciation`/*`\nVotre travail est très prenant, mais il vous enrichit énormément. Il a beaucoup de sens pour vous, vous avez le sentiment de faire avancer votre domaine. Vos chefs sont très contents de votre travail. C’est gratifiant!
+    Les années passent et votre importance grandit au sein de la boîte. Vous avez gagné en responsabilités mais votre salaire n’a pas évolué en conséquence. \n\n\n\nLes bruits de couloir laissent aussi entendre qu’on vous paie moins que vos homologues masculins. “C’est la relève, il faut les garder dans l’entreprise.” Vous explique votre chef quand vous abordez timidement le sujet. Vous réalisez alors qu’on n’envisage même pas de vous promouvoir, vous! Presque sur un coup de tête, vous demandez à être promue. \n\n\n“Ah mais tu ne veux pas d’enfants? Ce sera vraiment dur de t’en occuper si tu deviens manager, c’est pour ça que je ne te l’ai jamais proposé. Je ne voulais pas te placer face à ce choix difficile!”
+    Vous clarifiez le malentendu et votre chef finit par vous octroyer le poste! Il a toujours cru en vos capacités. Les négociations salariales vous déçoivent un peu, mais vous vous estimez déjà chanceuse d’avoir obtenu le poste. \n\n\nVous prouverez qu’une femme peut faire le travail tout aussi bien qu’un homme! La reconnaissance salariale suivra.
+    Vous devenez donc cheffe de projet et prenez en charge la direction d’une petite équipe que vous connaissez bien, puisque ce sont vos anciens collègues. Vous êtes aux anges, votre carrière décolle enfin, après presque 10 ans de dur travail! \n\n\nMais vous déchantez vite. Ceux qui sont désormais vos collaborateurs changent de comportement. Cela commence par des réactions surprises et vexées à votre promotion. Ce sont tous des hommes, et ils vous font rapidement comprendre que la seule explication possible est d’être “passée sous la table” du chef. L’ambiance se détériore rapidement: ils ne vous écoutent pas, ils ne respectent pas vos décisions, ils vous excluent des réunions. \n“Il faut savoir s’imposer”, vous dit votre supérieur au moment où vous lui confiez le problème. “Les gars, faut leur montrer qui est l’alpha si on veut se faire respecter. Fait preuve d’autorité et de fermeté, ça passera.” Alors c’est ce que vous faites: vous vous grandissez, vous les réprimandez, vous parlez plus fort qu’eux. Mais c’est l’escalade: vous avez “un balais dans le cul”, vous êtes “chiante”, une “connasse”, une “mal baisée”.
+    La situation devient insupportable. Vous avez la boule au ventre en partant travailler, vous subissez stoïquement les insultes et les menaces pendant la journée, et vous vous effondrez le soir. Mathieu est désemparé. “Il faut les dénoncer! Tu peux pas te laisser traiter comme ça, c’est pas normal à notre époque!” Vous y réfléchissez sérieusement. Il faut que quelque chose change, c’est sûr, mais vous avez peur pour votre réputation et votre carrière...`;*/
 
-     let reponseOui = `La situation est inadmissible, vous devez vous battre bla bla.
-    \nAjouter 1) inefficacité
-    \n2) effets pervers
-    \n3) démission
-    \n Dénouement: impacts sociaux et psy`;
+     let reponseOui = `\nLa situation est inadmissible, vous devez vous battre! Après tout, vous êtes en bonne position pour changer le système. Aussi, vous savez que votre entreprise se targue d’avoir développé une cellule pour gérer les affaires de harcèlement. Vous la contactez discrètement, en évitant d’en parler à vos supérieurs. Les RH prêtent une oreille attentive à vos propos et vous êtes rassurée: ils vont mener un audit interne! \n\nVous reprenez espoir et continuez à travailler en attendant les résultats.
+     Six mois s’écoulent avant que le rapport d’audit ne soit rendu. Vous êtes à bout de nerfs à force de subir le harcèlement. Vous prenez régulièrement des arrêts maladie car les journées de travail vous semblent de plus en plus insurmontables, d’autant plus que vos collaborateurs vous soupçonnent de les avoir dénoncés. \n\n\nVotre productivité a chuté, tout comme celle de votre équipe. Votre supérieur se montre déçu et ne comprend pas ce qu’il passe (vous ne lui avez jamais parlé de l’audit). “Il va falloir sérieusement vous reprendre, Julie.”
+     A la maison, l’ambiance est tendue. Vous êtes très irritable et colérique. Votre relation avec Mathieu en prend un coup. Vous êtes trop fatiguée pour faire quoi que ce soit avec lui. \n\nIl se montre très patient mais n’a pas l’énergie de vous ramasser à la petite cuillère tous les soirs. Il sort avec ses amis et c’est tant mieux: vous préférez rester seule.
+     Mais l’audit est enfin terminé! Les RH vous font donc part de leurs conclusions: il n’y a pas assez de preuves pour prendre des mesures. “On ne peut rien faire de plus. C’est pas comme si on pouvait virer toute l’équipe. Vous avez pensé à chercher du travail ailleurs?” Vous êtes anéantie.
+     Vous démissionnez peu après. Le soulagement est immense mais éphémère. C’est tout votre investissement, toute votre vie qui perd son sens. La sidération vous plonge dans une léthargie et une tristesse profondes. Vous êtes incapable de chercher du travail pendant des mois. Retenter la même chose? A quoi bon? Vous avez l’impression de régresser. Le décalage avec votre entourage, avec le reste du monde, est considérable. \n\nVous ne comprenez pas comment vous, si combative et ambitieuse, en êtes arrivée là. Il vous faudra un an, et un bon psy, pour retrouver votre joie de vivre et de nouvelles aspirations professionnelles, bien différentes de celles que vous aviez nourries 15 ans auparavant…`;
 
-    let reponseNon = `La situation vous semble sans issue, 
-    \nvous n'avez plus confiance en votre hiérarchie, bla bla.
-    \n vous finissez par démissionner.
-    \n Dénouement: impacts sociaux et psy`;
+    let reponseNon = `\n\nLa situation vous semble sans issue. Vous vous êtes pliée en quatre pour faire fonctionner votre équipe, sans succès. Comment résoudre un problème qui ne vient pas de vous? Isolée et sans soutien, vous ne vous faites pas d’illusion. Vous savez que vos collaborateurs ne seront pas inquiétés par leurs agissements.
+    \n\n\n\nLe coeur lourd, et sans autre alternative, vous démissionnez. Le soulagement est immense mais éphémère. Vous êtes submergée par votre sentiment d’échec.  C’est tout votre investissement, toute votre vie qui perd son sens. La sidération vous plonge dans une léthargie et une tristesse profondes. Vous êtes incapable de chercher du travail pendant des mois. Retenter la même chose? A quoi bon? Vous avez l’impression de régresser.\n\n Le décalage avec votre entourage, avec le reste du monde, est considérable. Vous ne comprenez pas comment vous, si combative et ambitieuse, en êtes arrivée là. Il vous faudra un an, et un bon psy, pour retrouver votre joie de vivre et de nouvelles aspirations professionnelles, bien différentes de celles que vous aviez nourries 15 ans auparavant…`;
 
-    let toastTexte = `expliquer facteurs de ségrégation verticale
-    \n+ ajouter stats inégalités salariales
-    \n+ stat/info harcèlement`;
-      
+    let toastTexte1 = `En 2018, l’écart de salaire entre hommes et femmes \nest de 11.55% (secteurs public et privé confondus). \nIl monte à 18.55% chez les cadres supérieurs et moyens. \n(OFS, 2020)`;
+    let toastTexte2 = `En 2019 en Suisse, seules 35% des positions dirigeantes \nsont occupées par des femmes. (OFS, 2020a)`;
+    let toastTexte3 = `La "ségrégation verticale" persiste: notre société accorde \nune valeur symbolique et monétaire plus élevée au \ntravail des hommes et les favorise dans l’accès \naux postes prestigieux et bien rémunérés. \n(Kergoat, 2004; Bereni et al., 2012)`;
+
     let textBoxQuestion = creerTextBox(this, content, 16);
 
-    let boutonOui = creerBouton(this, false, 180, 480, 'DÉNONCER VOS COLLABORATEURS');
-    let boutonNon = creerBouton(this, false, 560, 480, 'DÉMISSIONNER');
+    let boutonOui = creerBouton(this, false, 220, 480, 'DÉNONCER VOS COLLABORATEURS');
+    let boutonNon = creerBouton(this, false, 600, 480, 'DÉMISSIONNER');
 
     textBoxQuestion.on('pageend', () => {
       if (textBoxQuestion.isLastPage) {
@@ -58,7 +54,9 @@ class ScenePromotion extends Phaser.Scene {
         16,
         boutonNon,
         'rejouer',
-        toastTexte
+        toastTexte1,
+        toastTexte2,
+        toastTexte3
       );
     });
 
@@ -72,7 +70,9 @@ class ScenePromotion extends Phaser.Scene {
         16,
         boutonOui,
         'rejouer',
-        toastTexte
+        toastTexte1,
+        toastTexte2,
+        toastTexte3
       );
     });
   }
